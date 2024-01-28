78342ce2c1748761b03fabf9a270731cdba7c825
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-28 13:06:13 -0600

    intiial commmit, created a basic entites and screens

---
+++ README.md
@@ -0,0 +1,6 @@
+# cinemapedia
+
+# Dev
+
+1. Copiar el .env.template y renombrarlo a .env
+2. Cambiar las variabels de entorno con tus credenciales de [TheMovieDB](https://themoviedb.org/)
---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+
+// class MenuItem {
+//   final String title;
+//   final String subtitle;
+//   final String link;
+//   final IconData icon;
+
+//   MenuItem({
+//     required this.title,
+//     required this.subtitle,
+//     required this.link,
+//     required this.icon
+//   });
+// }
+
+// const appMenuItems = <MenuItem>[
+//   MenuItem(title: '', subtitle: subtitle, link: link, icon: icon)
+// ];
