b72a177c4fa1ab959c3edcbe6ceda0bc8a6ecbbe
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-28 15:38:47 -0600

    added repository implementation

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -5,7 +5,7 @@ import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
-class MoviedbDatasource extends MovieDataSources {
+class MoviedbDatasource extends MoviesDataSource {
   final dio  = Dio(BaseOptions(
     baseUrl: 'https://api.themoviedb.org/3',
     queryParameters: {
