fba599e19afb51cd0bc2ea72c6784c6c8abe069e
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-29 16:12:50 -0600

    render all movies from the state

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -12,6 +14,42 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cinemapedia', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
+      body: const _HomeViewState(),
+    );
+  }
+}
+
+class _HomeViewState extends ConsumerStatefulWidget {
+  const _HomeViewState();
+
+  @override
+  _HomeViewStateState createState() => _HomeViewStateState();
+}
+
+class _HomeViewStateState extends ConsumerState<_HomeViewState> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+    if (nowPlayingMovies.length == 0) return const Center(child: CircularProgressIndicator());
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        
+        final movie = nowPlayingMovies[index];
+
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
     );
   }
 }
