a7df372670240ab54d3955eb3eeac715af35f670
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-29 19:58:18 -0600

    human format in the view count

--- lib/presentation/widget/movies/movie_horizontal_listview.dart
+++ lib/presentation/widget/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -78,14 +79,17 @@ class _Slide extends StatelessWidget {
             child: Text(movie.title, maxLines: 2, style: textStyles.titleSmall,),
           ),
 
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-              const SizedBox(height: 6,),
-              Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800),),
-              const SizedBox(height: 10,),
-              Text('${movie.popularity}', style: textStyles.bodySmall,)
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(width: 3,),
+                Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800),),
+                const Spacer(),
+                Text(HumanFormat.number(movie.popularity), style: textStyles.bodySmall,)
+              ],
+            ),
           )
         ],
       ),
