4bf3a1f197d52d7df15ccb2e66a4348355b3ed9a
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-29 19:50:06 -0600

    render the images, title. stars and popularity movie

--- lib/presentation/widget/movies/movie_horizontal_listview.dart
+++ lib/presentation/widget/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -23,6 +24,69 @@ class MovieHorizontalListview extends StatelessWidget {
         children: [
           if (title != null || subTitle != null)
             _Title(title: title, subTitle: subTitle),
+
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              itemCount: movies.length,
+              itemBuilder: (context, index) {
+                return _Slide(movie: movies[index],);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  
+  const _Slide({ required this.movie });
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 150,
+            child: Image.network(
+              fit: BoxFit.cover,
+              movie.posterPath,
+              width: 150,
+              loadingBuilder: (context, child, loadingProgress) {
+                if ( loadingProgress != null ) {
+                  return const Center(
+                    child: CircularProgressIndicator(strokeWidth: 2,)
+                  );
+                }
+                
+                return FadeIn(child: child);
+              },
+            ),
+          ),
+          const SizedBox(height: 5,),
+          SizedBox(
+            width: 150,
+            child: Text(movie.title, maxLines: 2, style: textStyles.titleSmall,),
+          ),
+
+          Row(
+            children: [
+              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+              const SizedBox(height: 6,),
+              Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800),),
+              const SizedBox(height: 10,),
+              Text('${movie.popularity}', style: textStyles.bodySmall,)
+            ],
+          )
         ],
       ),
     );
