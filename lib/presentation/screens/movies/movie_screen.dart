e21821702b460358f3447fb1400a4863299826e1
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-02-02 16:50:40 -0600

    added actor datasource, repository, mappers, etc

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -28,18 +28,86 @@ class MovieScrenState extends ConsumerState<MovieScren> {
   Widget build(BuildContext context) {
     final Movie? movie = ref.watch( movieInfoProvider )[widget.movieId];
 
-    if ( movie == null ) return  Center(child: CircularProgressIndicator( strokeWidth: 2, ));
+    if ( movie == null ) return  const Center(child: CircularProgressIndicator( strokeWidth: 2, ));
 
     return Scaffold(
       // appBar: AppBar(
       //   title: Text('MovieId ${widget.movieId}'),
       // ),
       body: CustomScrollView(
-        physics: ClampingScrollPhysics(),
+        physics: const ClampingScrollPhysics(),
         slivers: [
-          _CustomSilverAppBar(movie: movie,)
+          _CustomSilverAppBar(movie: movie,),
+          SliverList(delegate: SliverChildBuilderDelegate(
+            (context, index) => _MovieDetails(movie: movie),
+            childCount: 1
+          ))
         ],
       )
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  
+  const _MovieDetails({
+    required this.movie
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+              const SizedBox(width: 10,),
+              SizedBox(
+               width: (size.width - 40) * 0.7, 
+               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(movie.title, style: textStyles.titleLarge,),
+                  Text(movie.overview,),
+                ],
+               ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: Chip(
+                  label: Text(gender),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  )
+                ),
+              ))
+            ],
+          ),
+        ),
+        
+        const SizedBox(height: 100,),
+      ],
     );
   }
 }
@@ -61,8 +129,47 @@ class _CustomSilverAppBar extends StatelessWidget {
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         title: Text(
           movie.title,
-          style: TextStyle(fontSize: 20),
+          style: const TextStyle(fontSize: 20),
           textAlign: TextAlign.start,
+        ),
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.7, 1],
+                    colors: [
+                      Colors.transparent,
+                      Colors.black87,
+                    ]
+                  )
+                ),
+              ),
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    stops: [0.0, 0.3],
+                    colors: [
+                      Colors.black87,
+                      Colors.transparent,
+                    ]
+                  )
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
