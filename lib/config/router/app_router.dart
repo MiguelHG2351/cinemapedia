f66d2d415e680178d4db7c684226cc9fa32b61f8
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-03-29 10:04:42 -0600

    added preserve state without shellRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,53 +4,39 @@ import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
+  initialLocation: '/home/0',
   routes: [
-    // GoRoute(
-    //   path: '/',
-    //   name: HomeScreen.name,
-    //   builder: (context, state) => const HomeScreen( childView: HomeView(), ),
-    //   routes: [
-    //     GoRoute(
-    //       path: 'movie/:id',
-    //       name: MovieScren.name,
-    //       builder: (context, state) {
-    //         final movieId = state.pathParameters['id'] ?? 'no-id';
-
-    //         return MovieScren(movieId: movieId);
-    //       },
-    //     )
-    //   ]
-    // )
-
-    ShellRoute(
-      builder: (context, state, child) {
-        return HomeScreen(childView: child);
+    GoRoute(
+      path: '/home/:page',
+      name: HomeScreen.name,
+      builder: (context, state) {
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+        return HomeScreen( pageIndex: pageIndex,);
       },
       routes: [
         GoRoute(
-          path: '/',
+          path: 'movie/:id',
+          name: MovieScren.name,
           builder: (context, state) {
-            return const HomeView();
-          },
-          routes: [
-            GoRoute(
-              path: 'movie/:id',
-              name: MovieScren.name,
-              builder: (context, state) {
-                final movieId = state.pathParameters['id'] ?? 'no-id';
+            final movieId = state.pathParameters['id'] ?? 'no-id';
 
-                return MovieScren(movieId: movieId);
-              },
-            ),
-          ]
-        ),
-        GoRoute(
-          path: '/favorites',
-          builder: (context, state) {
-            return const FavoritesView();
+            return MovieScren(movieId: movieId);
           },
-        ),
+        )
       ]
+    ),
+    GoRoute(
+      path: '/favorites',
+      builder: (context, state) {
+        return const FavoritesView();
+      },
+    ),
+
+    GoRoute(
+      path: '/',
+      redirect: (_, __) => '/home/0',
     )
+
+
   ]
 );
--- lib/presentation/widget/shared/custom_bottom_navigation.dart
+++ lib/presentation/widget/shared/custom_bottom_navigation.dart
@@ -2,38 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  final int currentIndex;
+  const CustomBottomNavigation({super.key, required this.currentIndex});
 
   @override
   Widget build(BuildContext context) {
-    int getCurrentIndex(BuildContext context) {
-      final String location = GoRouterState.of(context).matchedLocation;
-
-      switch (location) {
-        case '/':
-          return 0;
-        case '/categories':
-          return 1;
-        case '/favorites':
-          return 2;
-        default:
-          return 0;
-      }
-    }
 
     void onItemTapped(BuildContext context, int index) {
       switch (index) {
         case 0:
-          context.go('/');
+          context.go('/home/0');
           break;
         case 1:
-          context.go('/categories');
+          context.go('/home/0');
           break;
         case 2:
-          context.go('/favorites');
+          context.go('/home/2');
           break;
         default:
-          context.go('/');
+          context.go('/home/0');
           break;
       }
     }
@@ -41,7 +28,7 @@ class CustomBottomNavigation extends StatelessWidget {
     return BottomNavigationBar(
       elevation: 0,
       onTap: (value) => onItemTapped(context, value),
-      currentIndex: getCurrentIndex(context),
+      currentIndex: currentIndex,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max),
